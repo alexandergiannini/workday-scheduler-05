4e3b88f28136a64e43e4c1665631ac7b78d67e5d
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-01-22 12:54:54 -0800

    added color functionality to the rows AGAIN

--- script.js
+++ script.js
@@ -35,21 +35,6 @@ let row5pm = document.querySelector(".row-5pm")
 /////////variables declaring the time text itself
 let text9am = document.querySelector("#time-9am") ///maybe declare this value as a specific time??
 
-let nine9am = "9:00"
-//let nineeee9am = moment(nineeee9am)
-console.log(nine9am)
-
-let ten10am = "10:00"
-console.log(ten10am)
-
-let eleven11am = "11:00"
-console.log(eleven11am)
-///////
-
-
-
-
-
 let text10am = document.querySelector("#time-10am")
 
 let text11am = document.querySelector("#time-11am")
@@ -100,9 +85,47 @@ let saveWork5pm = document.querySelector("#save-button-5pm")
 
 let currentTime = document.querySelector("#current-time")
 let rightNow
-let now = moment().format('LLLL')
-//console.log(now)
 
+let displayDate = moment().format('LLLL') ///to display the time above the Work Day schedule header
+
+let now = new Date() //moment().format('LLLL')
+console.log(now)
+
+let nine9am = new Date()
+nine9am.setHours(9, 0, 0, 0)
+console.log(nine9am)
+
+let ten10am = new Date()
+ten10am.setHours(10, 0, 0, 0)
+console.log(ten10am)
+
+
+let eleven11am = new Date()
+eleven11am.setHours(11, 0, 0, 0)
+console.log(eleven11am)
+
+let twelve12pm = new Date()
+twelve12pm.setHours(12, 0, 0, 0)
+console.log(twelve12pm)
+///////
+
+let one1pm = new Date()
+one1pm.setHours(13, 0, 0, 0)
+console.log(one1pm)
+
+let two2pm = new Date()
+two2pm.setHours(14, 0, 0, 0)
+console.log(two2pm)
+
+let three3pm = new Date()
+three3pm.setHours(15, 0, 0, 0)
+
+let four4pm = new Date()
+four4pm.setHours(16, 0, 0, 0)
+
+let five5pm = new Date()
+five5pm.setHours(17, 0, 0, 0)
+console.log(five5pm)
 
 let timeColorBlock = document.querySelectorAll("time")
 
@@ -113,22 +136,116 @@ let myTime = true
 //function to display the current time
 let displayCurrentTime = function () {
     rightNow = document.createElement('p')
-    rightNow.textContent = now
+    rightNow.textContent = displayDate
     currentTime.parentNode.replaceChild(rightNow, currentTime)
 }
 
 ///maybe distguish even function separately based on time?
-let changeTimeColor = function () {
-    if (rightNow > ten10am) {
-       row10am.setAttribute('style', 'background-color: #d3d3d3')
-   } else if (rightNow > nine9am) {
-       row9am.setAttribute('style', 'background-color: #d3d3d3')
-   } else if (rightNow > eleven11am) {
-       row11am.setAttribute('style', 'background-color: #d3d3d3')
-   }
-    
+let change9amTimeColor = function () {
+  if (now > nine9am) {
+      row9am.setAttribute('style', 'background-color: #d3d3d3')
+  } else if (now < nine9am) {
+      row9am.setAttribute('style', 'background-color: #77dd77')
+  } else { ////may have to adjust these else statement to reset the color of the rowss
+      row9am.setAttribute('style', 'background-color: #ff6961')
+  }
 
 }
+
+let change10amTimeColor = function () {
+    if (now > ten10am) {
+        row10am.setAttribute('style', 'background-color: #d3d3d3')
+    } else if (now < ten10am) {
+        row10am.setAttribute('style', 'background-color: #77dd77')
+    } else {
+        row10am.setAttribute('style', 'background-color: #ff6961')
+    }
+  
+  }
+
+
+  let change11amTimeColor = function () {
+    if (now > eleven11am) {
+        row11am.setAttribute('style', 'background-color: #d3d3d3')
+    } else if (now < eleven11am) {
+        row11am.setAttribute('style', 'background-color: #77dd77')
+    } else {
+        row11am.setAttribute('style', 'background-color: #ff6961')
+    }
+  
+  }
+
+
+  let change12pmTimeColor = function () {
+    if (now > twelve12pm) {
+        row12pm.setAttribute('style', 'background-color: #d3d3d3')
+    } else if (now < twelve12pm) {
+        row12pm.setAttribute('style', 'background-color: #77dd77')
+    } else {
+        row12pm.setAttribute('style', 'background-color: #ff6961')
+    }
+  
+  }
+
+  let change1pmTimeColor = function () {
+    if (now > one1pm) {
+        row1pm.setAttribute('style', 'background-color: #d3d3d3')
+    } else if (now < one1pm) {
+        row1pm.setAttribute('style', 'background-color: #77dd77')
+    } else {
+        row1pm.setAttribute('style', 'background-color: #ff6961')
+    }
+  
+  }
+
+  let change2pmTimeColor = function () {
+    if (now > two2pm) {
+        row2pm.setAttribute('style', 'background-color: #d3d3d3')
+    } else if (now < two2pm) {
+        row2pm.setAttribute('style', 'background-color: #77dd77')
+    } else {
+        row2pm.setAttribute('style', 'background-color: #ff6961')
+    }
+  
+  }
+
+  let change3pmTimeColor = function () {
+    if (now > three3pm) {
+        row3pm.setAttribute('style', 'background-color: #d3d3d3')
+    } else if (now < three3pm) {
+        row3pm.setAttribute('style', 'background-color: #77dd77')
+    } else {
+        row3pm.setAttribute('style', 'background-color: #ff6961')
+    }
+  
+  }
+
+  let change4pmTimeColor = function () {
+    if (now > four4pm) {
+        row4pm.setAttribute('style', 'background-color: #d3d3d3')
+    } else if (now < four4pm) {
+        row4pm.setAttribute('style', 'background-color: #77dd77')
+    } else {
+        row4pm.setAttribute('style', 'background-color: #ff6961')
+    }
+  
+  }
+
+  let change5pmTimeColor = function () {
+    if (now > five5pm) {
+        row5pm.setAttribute('style', 'background-color: #d3d3d3')
+    } else if (now < five5pm) {
+        row5pm.setAttribute('style', 'background-color: #77dd77')
+    } else {
+        row5pm.setAttribute('style', 'background-color: #ff6961')
+    }
+  
+  }
+
+
+
+
+
 
 workText9am.onclick = function () {
     editedWorkText = document.createElement('input')
@@ -291,4 +408,20 @@ saveWork10am.onclick = function (event) {
 
 displayCurrentTime() //calling function to display current time
 
-changeTimeColor() ///calling function to change the row color based on time INCOMPLETE!!
+change9amTimeColor() ///calling function to change the row color based on time INCOMPLETE!!
+
+change10amTimeColor()
+
+change11amTimeColor()
+
+change12pmTimeColor()
+
+change1pmTimeColor()
+
+change2pmTimeColor()
+
+change3pmTimeColor()
+
+change4pmTimeColor()
+
+change5pmTimeColor()
