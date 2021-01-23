f00e95ac327df7d42e6915a841d56995f08d8127
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-01-22 21:28:13 -0800

    finished adding local storage functionality

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-///createText function
-//editText function
-///saveTextfunction
-///getTextfunction
-///time functions
-
 let body = document.body 
 let editedWorkText
 
@@ -33,7 +26,8 @@ let row5pm = document.querySelector(".row-5pm")
 
 
 /////////variables declaring the time text itself
-let text9am = document.querySelector("#time-9am") ///maybe declare this value as a specific time??
+let text9am = document.querySelector("#time-9am") 
+///maybe declare this value as a specific time??
 
 let text10am = document.querySelector("#time-10am")
 
@@ -63,7 +57,6 @@ let entireContainer = document.querySelector("container")
 let savedWork
 
 let workText9am = document.querySelector("#work-text-9am")
-console.log(workText9am)
 let workText10am = document.querySelector("#work-text-10am")
 let workText11am = document.querySelector("#work-text-11am")
 let workText12pm = document.querySelector("#work-text-12pm")
@@ -89,33 +82,26 @@ let rightNow
 let displayDate = moment().format('LLLL') ///to display the time above the Work Day schedule header
 
 let now = new Date() //moment().format('LLLL')
-console.log(now)
 
 let nine9am = new Date()
 nine9am.setHours(9, 0, 0, 0)
-console.log(nine9am)
 
 let ten10am = new Date()
 ten10am.setHours(10, 0, 0, 0)
-console.log(ten10am)
 
 
 let eleven11am = new Date()
 eleven11am.setHours(11, 0, 0, 0)
-console.log(eleven11am)
 
 let twelve12pm = new Date()
 twelve12pm.setHours(12, 0, 0, 0)
-console.log(twelve12pm)
 ///////
 
 let one1pm = new Date()
 one1pm.setHours(13, 0, 0, 0)
-console.log(one1pm)
 
 let two2pm = new Date()
 two2pm.setHours(14, 0, 0, 0)
-console.log(two2pm)
 
 let three3pm = new Date()
 three3pm.setHours(15, 0, 0, 0)
@@ -125,13 +111,9 @@ four4pm.setHours(16, 0, 0, 0)
 
 let five5pm = new Date()
 five5pm.setHours(17, 0, 0, 0)
-console.log(five5pm)
 
 let timeColorBlock = document.querySelectorAll("time")
 
-
-//using this as an example
-let myTime = true
 
 //function to display the current time
 let displayCurrentTime = function () {
@@ -141,107 +123,200 @@ let displayCurrentTime = function () {
 }
 
 ///maybe distguish even function separately based on time?
+////may need to add an else clause just to turn the rows white when the clock is at a time which isnt displayed on the planner
 let change9amTimeColor = function () {
-  if (now > nine9am) {
-      row9am.setAttribute('style', 'background-color: #d3d3d3')
+  if (now.getHours() === nine9am.getHours()) {
+      row9am.setAttribute('style', 'background-color: #ff6961')
   } else if (now < nine9am) {
       row9am.setAttribute('style', 'background-color: #77dd77')
-  } else { ////may have to adjust these else statement to reset the color of the rowss
-      row9am.setAttribute('style', 'background-color: #ff6961')
+  } else if (now > nine9am){ ////may have to adjust these else statement to reset the color of the rowss
+      row9am.setAttribute('style', 'background-color: #d3d3d3')
   }
-
+//
 }
 
 let change10amTimeColor = function () {
-    if (now > ten10am) {
-        row10am.setAttribute('style', 'background-color: #d3d3d3')
+    if (now.getHours() === ten10am.getHours()) {
+        row10am.setAttribute('style', 'background-color: #ff6961')
     } else if (now < ten10am) {
         row10am.setAttribute('style', 'background-color: #77dd77')
-    } else {
-        row10am.setAttribute('style', 'background-color: #ff6961')
+    } else if (now > ten10am) {
+        row10am.setAttribute('style', 'background-color: #d3d3d3')
     }
   
   }
 
 
   let change11amTimeColor = function () {
-    if (now > eleven11am) {
-        row11am.setAttribute('style', 'background-color: #d3d3d3')
+    if (now.getHours() === eleven11am.getHours()) {
+        row11am.setAttribute('style', 'background-color: #ff6961')
     } else if (now < eleven11am) {
         row11am.setAttribute('style', 'background-color: #77dd77')
-    } else {
-        row11am.setAttribute('style', 'background-color: #ff6961')
+    } else if (now > eleven11am) {
+        row11am.setAttribute('style', 'background-color: #d3d3d3')
     }
   
   }
 
 
   let change12pmTimeColor = function () {
-    if (now > twelve12pm) {
-        row12pm.setAttribute('style', 'background-color: #d3d3d3')
+    if (now.getHours() === twelve12pm.getHours()) {
+        row12pm.setAttribute('style', 'background-color: #ff6961')
     } else if (now < twelve12pm) {
         row12pm.setAttribute('style', 'background-color: #77dd77')
-    } else {
-        row12pm.setAttribute('style', 'background-color: #ff6961')
+    } else if (now > twelve12pm) {
+        row12pm.setAttribute('style', 'background-color: #d3d3d3')
     }
   
   }
 
   let change1pmTimeColor = function () {
-    if (now > one1pm) {
-        row1pm.setAttribute('style', 'background-color: #d3d3d3')
+    if (now.getHours() === one1pm.getHours()) {
+        row1pm.setAttribute('style', 'background-color: #ff6961')
     } else if (now < one1pm) {
         row1pm.setAttribute('style', 'background-color: #77dd77')
-    } else {
-        row1pm.setAttribute('style', 'background-color: #ff6961')
+    } else if (now > one1pm) {
+        row1pm.setAttribute('style', 'background-color: #d3d3d3')
     }
   
   }
 
   let change2pmTimeColor = function () {
-    if (now > two2pm) {
-        row2pm.setAttribute('style', 'background-color: #d3d3d3')
+    if (now.getHours() === two2pm.getHours()) {
+        row2pm.setAttribute('style', 'background-color: #ff6961')
     } else if (now < two2pm) {
         row2pm.setAttribute('style', 'background-color: #77dd77')
-    } else {
-        row2pm.setAttribute('style', 'background-color: #ff6961')
+    } else if (now > two2pm) {
+        row2pm.setAttribute('style', 'background-color: #d3d3d3')
     }
   
   }
 
   let change3pmTimeColor = function () {
-    if (now > three3pm) {
-        row3pm.setAttribute('style', 'background-color: #d3d3d3')
+    if (now.getHours() === three3pm.getHours()) {
+        row3pm.setAttribute('style', 'background-color: #ff6961')
     } else if (now < three3pm) {
         row3pm.setAttribute('style', 'background-color: #77dd77')
-    } else {
-        row3pm.setAttribute('style', 'background-color: #ff6961')
+    } else if (now > three3pm) {
+        row3pm.setAttribute('style', 'background-color: #d3d3d3')
     }
   
   }
 
   let change4pmTimeColor = function () {
-    if (now > four4pm) {
-        row4pm.setAttribute('style', 'background-color: #d3d3d3')
+    if (now.getHours() === four4pm.getHours()) {
+        row4pm.setAttribute('style', 'background-color: #ff6961')
     } else if (now < four4pm) {
         row4pm.setAttribute('style', 'background-color: #77dd77')
-    } else {
-        row4pm.setAttribute('style', 'background-color: #ff6961')
+    } else if (now > four4pm) {
+        row4pm.setAttribute('style', 'background-color: #d3d3d3')
     }
   
   }
 
   let change5pmTimeColor = function () {
-    if (now > five5pm) {
-        row5pm.setAttribute('style', 'background-color: #d3d3d3')
+    if (now.getHours() === five5pm.getHours()) {
+        row5pm.setAttribute('style', 'background-color: #ff6961')
     } else if (now < five5pm) {
         row5pm.setAttribute('style', 'background-color: #77dd77')
-    } else {
-        row5pm.setAttribute('style', 'background-color: #ff6961')
+    } else if (now > five5pm) {
+        row5pm.setAttribute('style', 'background-color: #d3d3d3')
     }
   
   }
 
+  let saveStorage9am = function () {
+      localStorage.setItem("workText9am", JSON.stringify(savedWork9am)) //workText9am may have to be in quotes or declared as a variable
+  }
+
+let loadStorage9am = function () {
+    let myText = localStorage.getItem("workText9am")
+    console.log(myText)
+    return myText.replace(/[""]/gi, '');
+}
+
+
+let saveStorage10am = function () {
+    localStorage.setItem("workText10am", JSON.stringify(savedWork10am)) 
+}
+
+let loadStorage10am = function () {
+     let myText = localStorage.getItem("workText10am")
+     console.log(myText)
+     return myText.replace(/[""]/gi, '');
+}
+
+let saveStorage11am = function () {
+    localStorage.setItem("workText11am", JSON.stringify(savedWork11am)) 
+}
+
+let loadStorage11am = function () {
+     let myText = localStorage.getItem("workText11am")
+     console.log(myText)
+     return myText.replace(/[""]/gi, '');
+}
+
+
+let saveStorage12pm = function () {
+    localStorage.setItem("workText12pm", JSON.stringify(savedWork12pm)) 
+}
+
+let loadStorage12pm = function () {
+     let myText = localStorage.getItem("workText12pm")
+     console.log(myText)
+     return myText.replace(/[""]/gi, '');
+}
+
+let saveStorage1pm = function () {
+    localStorage.setItem("workText1pm", JSON.stringify(savedWork1pm)) 
+}
+
+let loadStorage1pm = function () {
+     let myText = localStorage.getItem("workText1pm")
+     console.log(myText)
+     return myText.replace(/[""]/gi, '');
+}
+
+let saveStorage2pm = function () {
+    localStorage.setItem("workText2pm", JSON.stringify(savedWork2pm)) 
+}
+
+let loadStorage2pm = function () {
+     let myText = localStorage.getItem("workText2pm")
+     console.log(myText)
+     return myText.replace(/[""]/gi, '');
+}
+
+let saveStorage3pm = function () {
+    localStorage.setItem("workText3pm", JSON.stringify(savedWork3pm)) 
+}
+
+let loadStorage3pm = function () {
+     let myText = localStorage.getItem("workText3pm")
+     console.log(myText)
+     return myText.replace(/[""]/gi, '');
+}
+
+
+let saveStorage4pm = function () {
+    localStorage.setItem("workText4pm", JSON.stringify(savedWork4pm)) 
+}
+
+let loadStorage4pm = function () {
+     let myText = localStorage.getItem("workText4pm")
+     console.log(myText) ///this is null until i save/load data into localstorage
+     return myText.replace(/[""]/gi, '');
+}
+
+let saveStorage5pm = function () {
+    localStorage.setItem("workText5pm", JSON.stringify(savedWork5pm)) 
+}
+
+let loadStorage5pm = function () {
+     let myText = localStorage.getItem("workText5pm")
+     console.log(myText) ///this is null until i save/load data into localstorage
+     return myText.replace(/[""]/gi, '');
+}
 
 
 
@@ -305,8 +380,16 @@ saveWork9am.onclick = function (event) {
   //event.preventDefault()
   savedWork9am = editedWorkText.value
   workText9am = document.createElement("p")
+  workText9am.setAttribute('id', 'work-text-9am')
   workText9am.textContent = savedWork9am
   editedWorkText.parentNode.replaceChild(workText9am, editedWorkText)
+  saveStorage9am()
+
+  //// this correctly loads the value
+  console.log(editedWorkText.value)
+/// below doesnt work
+//workText9am.textContent = loadStorage9am()
+
 }
 
 saveWork10am.onclick = function (event) {
@@ -315,6 +398,7 @@ saveWork10am.onclick = function (event) {
     workText10am = document.createElement("p")
     workText10am.textContent = savedWork10am
     editedWorkText.parentNode.replaceChild(workText10am, editedWorkText)
+    saveStorage10am()
   }
 
   saveWork11am.onclick = function (event) {
@@ -323,6 +407,7 @@ saveWork10am.onclick = function (event) {
     workText11am = document.createElement("p")
     workText11am.textContent = savedWork11am
     editedWorkText.parentNode.replaceChild(workText11am, editedWorkText)
+    saveStorage11am()
   }
 
   saveWork12pm.onclick = function (event) {
@@ -331,6 +416,7 @@ saveWork10am.onclick = function (event) {
     workText12pm = document.createElement("p")
     workText12pm.textContent = savedWork12pm
     editedWorkText.parentNode.replaceChild(workText12pm, editedWorkText)
+    saveStorage12pm()
   }
 
   saveWork1pm.onclick = function (event) {
@@ -339,6 +425,7 @@ saveWork10am.onclick = function (event) {
     workText1pm = document.createElement("p")
     workText1pm.textContent = savedWork1pm
     editedWorkText.parentNode.replaceChild(workText1pm, editedWorkText)
+    saveStorage1pm()
   }
 
   saveWork2pm.onclick = function (event) {
@@ -347,6 +434,7 @@ saveWork10am.onclick = function (event) {
     workText2pm = document.createElement("p")
     workText2pm.textContent = savedWork2pm
     editedWorkText.parentNode.replaceChild(workText2pm, editedWorkText)
+    saveStorage2pm()
   }
 
   saveWork3pm.onclick = function (event) {
@@ -355,6 +443,7 @@ saveWork10am.onclick = function (event) {
     workText3pm = document.createElement("p")
     workText3pm.textContent = savedWork3pm
     editedWorkText.parentNode.replaceChild(workText3pm, editedWorkText)
+    saveStorage3pm()
   }
 
   saveWork4pm.onclick = function (event) {
@@ -363,6 +452,7 @@ saveWork10am.onclick = function (event) {
     workText4pm = document.createElement("p")
     workText4pm.textContent = savedWork4pm
     editedWorkText.parentNode.replaceChild(workText4pm, editedWorkText)
+    saveStorage4pm()
   }
 
   saveWork5pm.onclick = function (event) {
@@ -371,6 +461,7 @@ saveWork10am.onclick = function (event) {
     workText5pm = document.createElement("p")
     workText5pm.textContent = savedWork5pm
     editedWorkText.parentNode.replaceChild(workText5pm, editedWorkText)
+    saveStorage5pm()
   }
 
 //let workText = document.querySelectorAll("#work-text")
@@ -406,6 +497,7 @@ saveWork10am.onclick = function (event) {
    // console.log('Hi')
 //})
 
+
 displayCurrentTime() //calling function to display current time
 
 change9amTimeColor() ///calling function to change the row color based on time INCOMPLETE!!
@@ -425,3 +517,25 @@ change3pmTimeColor()
 change4pmTimeColor()
 
 change5pmTimeColor()
+
+/////right now, i have to add each data set in this order, in order for the data to appear
+workText9am.textContent = loadStorage9am()
+
+workText10am.textContent = loadStorage10am()
+
+workText11am.textContent = loadStorage11am()
+
+workText12pm.textContent = loadStorage12pm()
+
+workText1pm.textContent = loadStorage1pm()
+
+workText2pm.textContent = loadStorage2pm()
+
+workText3pm.textContent = loadStorage3pm()
+
+workText4pm.textContent = loadStorage4pm()
+
+workText5pm.textContent = loadStorage5pm()
+
+
+
